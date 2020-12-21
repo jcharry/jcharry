3ff88398277d3678df3b3ff1bf0329a400921d2b
Jamie <jamie.charry@gmail.com>
2020-12-20 23:22:35 -0500

    voltus logo?

--- generate.js
+++ generate.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 
-const boxSize = 30
+const boxSize = 5
 const gutterSize = 0
-const gridSize = [20, 4]
+const gridSize = [20, 5]
+// const center =
 
-const width = gridSize[0] * boxSize + (gridSize[0] - 1) * gutterSize
-const height = gridSize[1] * boxSize + (gridSize[1] - 1) * gutterSize
+const width = 400
+const height = 150
 
 const frameSize = [width, height]
 
@@ -22,50 +23,80 @@ const makeColor = (i, j) => {
   // return color.map((c, i) => averageColor(c, mix[i]))
 }
 
+// function duplicate(v, times) {
+//   let res = []
+//   for (let i = 0; i < times; i++) {
+//     res = res.concat(v)
+//   }
+//   return res
+// }
+// let c = duplicate(
+//   [
+//     '#00429d',
+//     '#2e59a8',
+//     '#4771b2',
+//     '#5d8abd',
+//     '#73a2c6',
+//     '#8abccf',
+//     '#a5d5d8',
+//     '#c5eddf',
+//     '#ffffe0'
+//   ],
+//   10
+// )
+
 const rects = []
 const defs = []
-for (let i = 0; i < gridSize[0]; i++) {
-  for (let j = 0; j < gridSize[1]; j++) {
-    const x = i * boxSize + i * (gutterSize - 1)
-    const y = j * boxSize + j * (gutterSize - 1)
-    const startColor = `rgb(${makeColor(i, j).join(',')}, 0.5)`
-    const stopColor = `rgb(${makeColor(i, j).join(',')}, 0.5)`
-    const randomDuration = 2000
-    const begin = (i + j) * 250
-    rects.push(
-      `<rect width="${boxSize}px" height="${boxSize}px" x="${x}px" y="${y}px" fill="${startColor}" filter="url(#blurMe)">
-        <animateTransform attributeName="transform" type="scale" values="1;1.5;1" begin="${begin}ms" dur="2000ms" repeatCount="indefinite"/>
-      </rect>`
-    )
-    // rects.push(
-    //   `<circle r="${boxSize /
-    //     2}px" cx="${x}px" cy="${y}px" fill="${startColor}" filter="url(#blurMe)">
-    //     <animateTransform attributeName="transform" type="scale" values="1;1.5;1" begin="${begin}ms" dur="2000ms" repeatCount="indefinite"/>
-    //   </circle>`
-    // )
+for (let r = 5, theta = 0; r < 100; r += 1, theta += Math.PI / 12) {
+  let x = r * Math.cos(theta)
+  let y = r * Math.sin(theta)
+  // const x = i * boxSize + i * (gutterSize - 1)
+  // const y = j * boxSize + j * (gutterSize - 1)
+  const startColor = `rgb(${makeColor(x, y).join(',')}, 0.5)`
+  // const stopColor = `rgb(${makeColor(i, j).join(',')}, 0.5)`
+  // const randomDuration = 2000
+  const begin = r * 100
+  // rects.push(
+  //   `<rect width="${boxSize}px" height="${boxSize}px" x="${x}px" y="${y}px" fill="${startColor}" filter="url(#blurMe)">
+  //     <animateTransform attributeName="transform" type="scale" values="1;1.5;1" begin="${begin}ms" dur="2000ms" repeatCount="indefinite"/>
+  //   </rect>`
+  // )
+  rects.push(
+    `<circle r="${r / 10}" cx="${x + width / 2}" cy="${y + height / 2}" fill="${
+      r % 2 === 0 ? 'black' : 'lightgray'
+    }" filter="url(#blurMe)">
+        <animateTransform attributeName="transform" type="scale" values="1;1.05;1" begin="${begin}ms" dur="2000ms" repeatCount="indefinite"/>
+      </circle>`
+  )
+  // rects.push(
+  //   `<circle r="${boxSize /
+  //     2}px" cx="${x}px" cy="${y}px" fill="${startColor}" filter="url(#blurMe)">
+  //     <animateTransform attributeName="transform" type="scale" values="1;1.5;1" begin="${begin}ms" dur="2000ms" repeatCount="indefinite"/>
+  //   </circle>`
+  // )
 
-    //     defs.push(`
-    //
-    // <linearGradient id="${x}-${y}">
-    //   <stop offset="0%" stop-color="${startColor}">
-    //     <animate
-    //       attributeName="offset"
-    //       dur="${randomDuration}ms"
-    //       values="0;0.1;0.2;0.3;0.4"
-    //       repeatCount="indefinite"
-    //     />
-    //   </stop>
-    //   <stop offset="100%" stop-color="${stopColor}">
-    //     <animate
-    //       attributeName="offset"
-    //       dur="${randomDuration}ms"
-    //       values="0.3;0.7;0.8;0.9;1"
-    //       repeatCount="indefinite"
-    //     />
-    //   </stop>
-    // </linearGradient>
-    //     `)
-  }
+  //     defs.push(`
+  //
+  // <linearGradient id="${x}-${y}">
+  //   <stop offset="0%" stop-color="${startColor}">
+  //     <animate
+  //       attributeName="offset"
+  //       dur="${randomDuration}ms"
+  //       values="0;0.1;0.2;0.3;0.4"
+  //       repeatCount="indefinite"
+  //     />
+  //   </stop>
+  //   <stop offset="100%" stop-color="${stopColor}">
+  //     <animate
+  //       attributeName="offset"
+  //       dur="${randomDuration}ms"
+  //       values="0.3;0.7;0.8;0.9;1"
+  //       repeatCount="indefinite"
+  //     />
+  //   </stop>
+  // </linearGradient>
+  //     `)
+  // }
 }
 
 const svg = `
